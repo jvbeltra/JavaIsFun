c6a4865bff9dbcca2bfc681c3ab7481c05905d99
AlfredSpidey <alfredo.vieira.neto90@gmail.com>
2017-05-28 00:44:18 -0300

    commit do alfred 4

--- app/src/main/java/com/example/dell_pc/javaisfun/HardActivity.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/HardActivity.java
@@ -1,5 +1,7 @@
 package com.example.dell_pc.javaisfun;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +14,25 @@ public class HardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard);
     }
-    public void TelaPrincipal(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    public void TextDialog(View view){
+        AlertDialog.Builder builder;
+
+        builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Home")
+                .setMessage("Você tem certeza que quer voltar ao menu principal?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(HardActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
