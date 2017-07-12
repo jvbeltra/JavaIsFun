b9afa1cc7acb9846d7cd0eb65cea01e5e57c74b3
AlfredSpidey <alfredo.vieira.neto90@gmail.com>
2017-07-12 00:25:20 -0300

    commit do alfred 13(Com um avanço nas variáveis, tabela, esqueminha de fade)

--- app/src/main/java/com/example/dell_pc/javaisfun/Variaveis2.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/Variaveis2.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Variaveis_2 extends AppCompatActivity {
+public class Variaveis2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_variaveis_2);
+        setContentView(R.layout.activity_variaveis2);
     }
     public void TextDialog(View view){
         AlertDialog.Builder builder;
@@ -23,7 +23,7 @@ public class Variaveis_2 extends AppCompatActivity {
                 .setMessage("Você tem certeza que quer voltar ao menu principal?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Variaveis_2.this, MainActivity.class);
+                        Intent intent = new Intent(Variaveis2.this, MainActivity.class);
                         startActivity(intent);
                     }
                 })
@@ -34,5 +34,9 @@ public class Variaveis_2 extends AppCompatActivity {
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+    }
+    public void next(View view){
+        Intent intent = new Intent(this, Variaveis3.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dell_pc/javaisfun/Variaveis3.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/Variaveis3.java
@@ -5,14 +5,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class Variaveis extends AppCompatActivity {
+public class Variaveis3 extends AppCompatActivity {
+    public TextView textView;
+    public ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_variaveis);
+        setContentView(R.layout.activity_variaveis3);
+        findViewById(R.id.textView7).setVisibility(View.GONE);
+        findViewById(R.id.imageButton2).setVisibility(View.GONE);
+
     }
     public void TextDialog(View view){
         AlertDialog.Builder builder;
@@ -23,7 +31,7 @@ public class Variaveis extends AppCompatActivity {
                 .setMessage("Você tem certeza que quer voltar ao menu principal?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Variaveis.this, MainActivity.class);
+                        Intent intent = new Intent(Variaveis3.this, MainActivity.class);
                         startActivity(intent);
                     }
                 })
@@ -35,8 +43,33 @@ public class Variaveis extends AppCompatActivity {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+
+
+            findViewById(R.id.textView7).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.imageButton2).setVisibility(View.VISIBLE);
+
+
+
+
+
+        return super.onTouchEvent(event);
+
+    }
     public void next(View view){
-        Intent intent = new Intent(this, Variaveis2.class);
+        Intent intent = new Intent(this, Variaveis4.class);
         startActivity(intent);
     }
+
+
+
+
+
+
+
 }
