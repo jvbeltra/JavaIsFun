2bdca89f7c3b69436ef0729cc2bb0e767935035f
Joao Vitor Beltramini <jvbeltra2011@gmail.com>
2017-07-18 21:06:41 -0300

    operadores quase completo

--- app/src/main/java/com/example/dell_pc/javaisfun/Operadores7.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/Operadores7.java
@@ -2,6 +2,7 @@ package com.example.dell_pc.javaisfun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Operadores7 extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Operadores7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operadores7);
+    }
+
+    public void checar(View view){
+
     }
 }
