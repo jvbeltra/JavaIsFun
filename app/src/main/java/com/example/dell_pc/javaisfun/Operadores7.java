ded35f64fe4334330e24223c8c817aea69ec892e
jvbeltra <jvbeltra2011@gmail.com>
2017-07-26 18:28:06 -0300

    op

--- app/src/main/java/com/example/dell_pc/javaisfun/Operadores7.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/Operadores7.java
@@ -10,9 +10,22 @@ public class Operadores7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operadores7);
+        findViewById(R.id.imageButton36).setVisibility(View.GONE);
+        findViewById(R.id.textErrado).setVisibility(View.GONE);
+        findViewById(R.id.textCorreto).setVisibility(View.GONE);
+        findViewById(R.id.maIgual).setVisibility(View.GONE);
     }
 
-    public void checar(View view){
-
+    public void correto(View view){
+        findViewById(R.id.imageButton36).setVisibility(View.VISIBLE);
+        findViewById(R.id.textCorreto).setVisibility(View.VISIBLE);
+        findViewById(R.id.maIgual).setVisibility(View.GONE);
+        findViewById(R.id.textErrado).setVisibility(View.GONE);
+    }
+    public void errado(View view){
+        findViewById(R.id.textCorreto).setVisibility(View.GONE);
+        findViewById(R.id.imageButton36).setVisibility(View.VISIBLE);
+        findViewById(R.id.textErrado).setVisibility(View.VISIBLE);
+        findViewById(R.id.maIgual).setVisibility(View.VISIBLE);
     }
 }
