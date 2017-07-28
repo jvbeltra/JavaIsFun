1a9e7a92f9315bfa5f304648bd79009037aa6665
AlfredSpidey <alfredo.vieira.neto90@gmail.com>
2017-07-28 14:11:02 -0300

    commit do alfred 21(O que é Java pronto)

--- app/src/main/java/com/example/dell_pc/javaisfun/OQueEJava2.java
+++ app/src/main/java/com/example/dell_pc/javaisfun/OQueEJava2.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
-public class OQueEJava extends AppCompatActivity {
+public class OQueEJava2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oque_ejava);
+        setContentView(R.layout.activity_oque_ejava2);
     }
     public void TextDialog(View view){
         AlertDialog.Builder builder;
@@ -23,7 +24,7 @@ public class OQueEJava extends AppCompatActivity {
                 .setMessage("Você tem certeza que quer voltar ao menu principal?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(OQueEJava.this, MainActivity.class);
+                        Intent intent = new Intent(OQueEJava2.this, MainActivity.class);
                         startActivity(intent);
                     }
                 })
@@ -36,7 +37,25 @@ public class OQueEJava extends AppCompatActivity {
                 .show();
     }
     public void next(View view){
-        Intent intent = new Intent(this, OQueEJava2.class);
+        Intent intent = new Intent(this, OQueEJava3.class);
         startActivity(intent);
+    }
+
+    public void imageDialog(View view){
+        AlertDialog.Builder builder;
+
+        builder = new AlertDialog.Builder(this);
+
+        ImageView imageView = new ImageView(this);
+
+        imageView.setImageResource(R.drawable.jvm);
+        builder.setTitle("Como funciona o JVM")
+                .setView(imageView)
+                .setNegativeButton("Voltar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
     }
 }
